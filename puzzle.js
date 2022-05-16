f971c4ba2b8c0abc7f38c14655d89d65751b8bdd
Geetanjali K <45900266+geetanjalikore@users.noreply.github.com>
2022-05-16 12:28:26 +0530

    Changed function name commitStatus to updateStatus
    changed files to fileNames

--- puzzle.js
+++ puzzle.js
@@ -29,21 +29,21 @@ const readFile = function (file) {
   return JSON.parse(readFileSync(file, 'utf8'));
 };
 
-const commitStatus = function (file, game) {
+const updateStatus = function (file, game) {
   writeFileSync(file, JSON.stringify(game), 'utf-8');
 };
 
 const main = function (move, { status, template, target }) {
   const game = readFile(status);
   play(game, move);
-  commitStatus(status, game);
+  updateStatus(status, game);
   generatePage(template, target, game);
 };
 
-const files = {
+const fileNames = {
   status: 'resources/gameStatus.json',
   template: 'resources/template.html',
   target: 'targetDir/puzzle.html'
 };
 
-main(process.argv[2], files);
+main(process.argv[2], fileNames);
